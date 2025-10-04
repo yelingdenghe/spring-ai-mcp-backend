e0cce5d1ddd44f55a9bbf04d429d4789d3472a7d
yling <yling@test.com>
2025-10-04 09:27:02 +0800

    切换qwen3-embedding-0.6B嵌入模型

--- mcp-client/src/main/java/com/yeling/config/ChatClientConfig.java
+++ mcp-client/src/main/java/com/yeling/config/ChatClientConfig.java
@@ -17,7 +17,6 @@ public class ChatClientConfig {
     @Bean
     ChatClient chatClient(ChatClient.Builder builder) {
         return builder.defaultSystem("你是一个聪明的ai，名字叫做夜凌")
-                .defaultSystem("基于上下文的知识库内容回答问题：")
                 .build();
     }
 }
