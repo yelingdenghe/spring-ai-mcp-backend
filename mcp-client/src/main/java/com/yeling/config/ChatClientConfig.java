d9f0dbb8f95134b2bafa924cc5d77f035b210e41
yling <yling@test.com>
2025-10-08 09:29:14 +0800

    实现基本的大模型将md转HTML与记忆会话功能

--- mcp-client/src/main/java/com/yeling/config/ChatClientConfig.java
+++ mcp-client/src/main/java/com/yeling/config/ChatClientConfig.java
@@ -1,7 +1,11 @@
 package com.yeling.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,9 +21,12 @@ import java.util.List;
 @Configuration
 public class ChatClientConfig {
 
+    private final ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
     @Bean
     ChatClient chatClient(ChatClient.Builder builder, ToolCallbackProvider tools) {
         return builder.defaultToolCallbacks(tools)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .defaultSystem("你是一个聪明的ai，名字叫做夜凌")
                 .build();
     }
--- mcp-server/src/main/java/com/yeling/mcp/tool/EmailTool.java
+++ mcp-server/src/main/java/com/yeling/mcp/tool/EmailTool.java
@@ -1,6 +1,10 @@
 
 package com.yeling.mcp.tool;
 
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 import com.yeling.entity.EmailRequest;
 import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
@@ -43,12 +47,42 @@ public class EmailTool {
             helper.setFrom(from);
             helper.setTo(email.getEmail());
             helper.setSubject(email.getSubject());
-            helper.setText(email.getMessage());
+            helper.setText(covert(email.getMessage()), true);
 
             mailSender.send(mimeMessage);
         } catch (Exception e) {
             log.error("====== 发送邮件出错：{} ======", e.getMessage());
         }
+    }
+
+    @Tool(description = "查询我的邮箱/邮件地址")
+    public String getMyEmailAddress() {
+        log.info("====== 调用MCP工具：getMyEmailAddress() ======");
+
+        return "1453443780@qq.com";
+    }
+
+    /**
+     * @description: MarkDown转HTML
+     * @author: 夜凌
+     * @date: 2025/10/8 07:50
+     * @param: [markdown]
+     * @return: String
+     **/
+    public static String covert(String markdown) {
+        // flexmark 的配置对象，用于设置 Markdown 解析和渲染的选项。这里创建一个空配置，表示使用默认规则
+        MutableDataSet options = new MutableDataSet();
+
+        // 根据传入的 options 配置构建一个 Markdown 解析器   Markdown → AST（抽象语法树） 的解析器。
+        Parser parser = Parser.builder(options).build();
+        // HtmlRenderer 是 AST → HTML 的渲染器   读取 Node（语法树），并输出标准 HTML。
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        // 把 Markdown 文本传给 parser.parse()，解析成一个 语法树结构（Node）
+        Node document = parser.parse(markdown);
+
+        // 把语法树交给渲染器 renderer，返回HTML字符串
+        return renderer.render(document);
     }
 
 }
